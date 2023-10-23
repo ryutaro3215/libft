9bf81713791bddd1bb40e20a7a2d4352db0ed0d5
ryutaro3215 <ryutaro140515@icloud.com>
2023-10-23 15:16:58 +0900

    modified ft_putendl

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmatsuba <rmatsuba@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: ryutaro320515 <ryutaro320515@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 17:14:03 by rmatsuba          #+#    #+#             */
-/*   Updated: 2023/10/20 21:34:51 by rmatsuba         ###   ########.fr       */
+/*   Updated: 2023/10/23 15:12:25 by ryutaro3205      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	int	i;
-
-	i = 0;
 	if (!s)
 		return ;
 	ft_putstr_fd(s, fd);
