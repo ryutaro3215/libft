609118a9f541782fc69d7e803097be1fd3eed130
ryutaro3215 <ryutaro140515@icloud.com>
2024-01-23 18:11:34 +0900

    add libft

---
+++ includes/get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ryutaro320515 <ryutaro320515@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 13:55:35 by rmatsuba          #+#    #+#             */
+/*   Updated: 2024/01/23 18:08:00 by ryutaro3205      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_read(int fd, char *keep_string);
+char	*ft_get_line(char *keep_string);
+char	*ft_keep_str(char *keep_string, char *result_string);
+char	*ft_free(char *str);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
